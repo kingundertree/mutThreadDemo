2838aade1c9d654766e44f3cf255294d7c9cac36
kingundertree <kingundertree@163.com>
2014-01-21 09:50:51 +0800

    新建mutThreadDemo项目，学习多线程方法
    多线程，GCD、NSThread, NSOperationQueue, NSInvocationOperation等；

---
+++ mutThreadDemo/DIAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  DIAppDelegate.h
+//  mutThreadDemo
+//
+//  Created by 夏至 on 14-1-21.
+//  Copyright (c) 2014年 dooioo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DIAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ mutThreadDemo/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  mutThreadDemo
+//
+//  Created by 夏至 on 14-1-21.
+//  Copyright (c) 2014年 dooioo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "DIAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([DIAppDelegate class]));
+    }
+}
